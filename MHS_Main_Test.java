c639bf985b6cf64e7c2fb467bb329118ed4ad1ae
JonasPersson78 <jonasnpersson@gmail.com>
2018-12-12 15:03:38 +0100

    Add comments for diffrent tests to do/ALL

--- MHS_Main_Test.java
+++ MHS_Main_Test.java
@@ -2,7 +2,7 @@ public class MHS_Main_Test {
 
   MHS_Main mhsMain = new MHS_Main();
 
-  // Test MHS_Main.checkPayment with non existing bank
+  // Test MHS_Main.checkPayment with non existing bank, Try with none existig 4 first digits. FAILED/FALSE and empty String. 
 
 
   // Test MHS_Main.checkPayment() with non existing account
@@ -18,7 +18,13 @@ public class MHS_Main_Test {
     return (payment.nameOfBank.equals("") && payment.isValid == false);
   }
 
-  // Test MHS_Main.checkPayment() with existing account with enough money
+  // Test MHS_Main.checkPayment() with existing account with too much money. PASS/TRUE OBS! Corret name of Bank.
+
+  // Test MHS_Main.checkPayment() with existing account with equal of money PASS/TRUE OBS! Corret name of Bank.
+
+  // Test MHS_Main.checkPayment() with existing account when ticketprice is 0 and balance is 0 PASS/TRUE OBS! Corret name of Bank.
+
+  // Test MHS_Main.checkPayment() Free ticket with none existing account. FAILED/FALSE and empty String.
 
   public static void main (String[] args) {
     MHS_Main_Test test = new MHS_Main_Test();
