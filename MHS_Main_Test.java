1d98be1586b1e251dacdc4485d6b67556dac1881
myhrnz1 <myhrnz@outlook.com>
2018-12-14 05:39:01 +0100

    comment out old way

--- MHS_Main_Test.java
+++ MHS_Main_Test.java
@@ -2,8 +2,9 @@ public class MHS_Main_Test {
 
   MHS_Main mhsMain = new MHS_Main();
 
-  // Test MHS_Main.checkPayment with non existing bank, Try with none existig 4 first digits. FAILED/FALSE and empty String.
+  /* old way
 
+  // Test MHS_Main.checkPayment with non existing bank, Try with none existig 4 first digits. FAILED/FALSE and empty String.
 
   // Test MHS_Main.checkPayment() with non existing account
   public boolean testCheckPaymentWithNonExistingAccount() {
@@ -13,14 +14,12 @@ public class MHS_Main_Test {
 
   // Test MHS_Main.checkPayment() with existing account with not enough money
   public boolean testCheckPaymentWithExistingAccount_NotEnoughMoney() {
-
     Payment payment = mhsMain.checkPayment("1234678912", 100);
     return (payment.nameOfBank.equals("") && payment.isValid == false);
   }
 
   // Test MHS_Main.checkPayment() with existing account with too much money. PASS/TRUE OBS! Corret name of Bank.
   public boolean testCheckPaymentWithExistingAccount_tooMuchMoney() {
-
     Payment payment = mhsMain.checkPayment("1234912345", 5000);
     return (payment.nameOfBank.equals("") && payment.isValid == true);
   }
@@ -29,10 +28,12 @@ public class MHS_Main_Test {
 
   // Test MHS_Main.checkPayment() with existing account when ticketprice is 0 and balance is 0 PASS/TRUE OBS! Corret name of Bank.
 
-  // Test MHS_Main.checkPayment() Free ticket with none existing account. FAILED/FALSE and empty String.
+  // Test MHS_Main.checkPayment() Free ticket with none existing account. FAILED/FALSE and empty String.*/
 
   public static void main (String[] args) {
     MHS_Main_Test test = new MHS_Main_Test();
+
+    /* old way
     if(test.testCheckPaymentWithExistingAccount_NotEnoughMoney()) {
       System.out.println("PASSED: " + "testCheckPaymentWithExistingAccount_NotEnoughMoney()");
     } else {
@@ -49,6 +50,6 @@ public class MHS_Main_Test {
     System.out.println("FAILED:  "+ "testCheckPaymentWithExistingAccount_tooMuchMoney");
     } else {
     System.out.println("PASSED:  "+ "testCheckPaymentWithExistingAccount_tooMuchMoney");
-    }
+  }*/
   }
 }
