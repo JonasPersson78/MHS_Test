8ba6f000ca3324fedf3e3f37e031253fd1407529
JonasPersson78 <45091643+JonasPersson78@users.noreply.github.com>
2018-12-14 13:01:44 +0100

    Delete MHS_BankAccount.java

--- MHS_BankAccount.java
+++
@@ -1,9 +0,0 @@
-public class MHS_BankAccount{
-  String accountNumber;
-  double balance;
-
-public MHS_BankAccount (String inputAccountNumber, double inputBalance){
-  accountNumber = inputAccountNumber;
-  balance = inputBalance;
-  }
-}
